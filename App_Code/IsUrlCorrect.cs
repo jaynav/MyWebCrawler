a3df5382dcaae51647032057744b58b63f58b277
javier <jaynav@outlook.com>
2013-10-14 21:54:05 -0700

    dumb git
    it did not include this file

---
+++ App_Code/IsUrlCorrect.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+public class IsUrlCorrect
+{
+    private string p;
+
+    public  IsUrlCorrect(string p)
+    {
+        // TODO: Complete member initialization
+        this.p = p;
+
+        
+    }
+
+    public string FixUrl()
+    {
+        
+            //(?:http://)|(?:https://)
+            if (Regex.IsMatch(p, @"\b(?:http://)|\b(?:https://)|\b(?:ftp://)"))
+            {
+                return p;
+            }
+            else
+            {
+                return p = "http://" + p;
+            }
+        }
+
+
+
+    public bool CheckValue()
+    {
+        if (p == "") 
+        {
+            return false;
+        }
+        return true;
+    }
+}
